7b6ecfae45654f81af1364c3f159ba7b5bbbcb4d
lxymahatma <326265648@qq.com>
2022-10-16 16:10:05 +0900

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,97 @@
+﻿using Assets.Scripts.PeroTools.Commons;
+using FormulaBase;
+using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+using Tomlet;
+using UnityEngine;
+
+namespace UIDisable
+{
+    public class Main : MelonMod
+    {
+        private static bool IsGameScene;
+        private static bool Set;
+
+        public override void OnApplicationStart()
+        {
+            Save.Load();
+            LoggerInstance.Msg("UI Disable Loaded");
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (sceneName == "GameMain")
+            {
+                IsGameScene = true;
+                Set = false;
+            }
+            else
+            {
+                IsGameScene = false;
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            if (IsGameScene)
+            {
+                if (!Save.Settings.ComboEnabled)
+                {
+                    if (GameObject.Find("Combo"))
+                    {
+                        GameObject.Find("Combo").SetActive(false);
+                    }
+                    else if (GameObject.Find("ComboGC"))
+                    {
+                        GameObject.Find("ComboGC").SetActive(false);
+                    }
+                    else if (GameObject.Find("Combo_djmax"))
+                    {
+                        GameObject.Find("Combo_djmax").SetActive(false);
+                    }
+                }
+
+                if (!Set)
+                {
+                    if (!Save.Settings.ScoreEnabled)
+                    {
+                        GameObject.Find("Score").SetActive(false);
+                    }
+                    if (!Save.Settings.BottomBarEnabled)
+                    {
+                        GameObject.Find("Below").SetActive(false);
+                    }
+                    if (!Save.Settings.HitPointEnabled)
+                    {
+                        GameObject.Find("HitPointRoad").SetActive(false);
+                        GameObject.Find("HitPointAir").SetActive(false);
+                    }
+                    if (!Save.Settings.ComboEnabled)
+                    {
+                        GameObject.Find("MultiHitComboControl").SetActive(false);
+                    }
+                    if (!Save.Settings.EffectEnabled)
+                    {
+                        GameObject.Find("Effects").SetActive(false);
+                    }
+                    if (!Save.Settings.PauseButtonEnabled)
+                    {
+                        GameObject.Find("BtnPause").SetActive(false);
+                    }
+                    Set = true;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.F9))
+            {
+                string Datas = File.ReadAllText(Path.Combine("UserData", "UI Disable.cfg"));
+                Save.Settings = TomletMain.To<Data>(Datas);
+            }
+        }
+    }
+}
---
+++ Save.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tomlet;
+using Tomlet.Attributes;
+using UnityEngine;
+
+namespace UIDisable
+{
+    internal class Save
+    {
+        private static Data Default = new Data(true, true, true, true, true, true);
+        internal static Data Settings;
+
+        public static void Load()
+        {
+            if (!File.Exists(Path.Combine("UserData", "UI Disable.cfg")))
+            {
+                string defaultConfig = TomletMain.TomlStringFrom(Default);
+                File.WriteAllText(Path.Combine("UserData", "UI Disable.cfg"), defaultConfig);
+            }
+            string Datas = File.ReadAllText(Path.Combine("UserData", "UI Disable.cfg"));
+            Settings = TomletMain.To<Data>(Datas);
+        }
+    }
+
+    internal struct Data
+    {
+        [TomlPrecedingComment("Disable Score GameObject or not")]
+        internal bool ScoreEnabled;
+
+        [TomlPrecedingComment("Disable Fever bar and HP or not")]
+        internal bool BottomBarEnabled;
+
+        [TomlPrecedingComment("Disable HitPoint or not")]
+        internal bool HitPointEnabled;
+
+        [TomlPrecedingComment("Disable Combo and hits count or not")]
+        internal bool ComboEnabled;
+
+        [TomlPrecedingComment("Disable all hit effects")]
+        internal bool EffectEnabled;
+
+        [TomlPrecedingComment("Disable pause button or not(you cannot pause if disable the button)")]
+        internal bool PauseButtonEnabled;
+
+        internal Data(bool scoreEnabled, bool bottomBarEnabled, bool hitPointEnabled, bool comboEnabled, bool effectEnabled, bool pauseButtonEnabled)
+        {
+            ScoreEnabled = scoreEnabled;
+            BottomBarEnabled = bottomBarEnabled;
+            HitPointEnabled = hitPointEnabled;
+            ComboEnabled = comboEnabled;
+            EffectEnabled = effectEnabled;
+            PauseButtonEnabled = pauseButtonEnabled;
+        }
+    }
+}
